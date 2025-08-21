16f046e05ff270da57c6e32e60a1d72a8317c61e
KANNAN-afkr <squora150@gmail.com>
2025-08-21 19:43:59 +0530

    Add gitignore

--- springapp/src/main/java/com/examly/springapp/controller/GiftController.java
+++ springapp/src/main/java/com/examly/springapp/controller/GiftController.java
@@ -16,7 +16,7 @@ public class GiftController {
     @Autowired
     private GiftService giftService;
     
-    @PostMapping("/addGift")
+    @PostMapping("/addGiftoo")
     public ResponseEntity<?> addGift(@RequestBody Gift gift) {
         try {
             Gift savedGift = giftService.addGift(gift);
